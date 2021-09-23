d255e53107077954a7dbcf241ac7c9b8ce0cc744
Kane Temblay <gerritvbart@gmail.com>
2021-09-23 19:45:08 -0300

    Bug fixes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'blank',
+  appId: 'com.kane.novacula',
+  appName: 'Novacula',
   webDir: 'www',
   bundledWebRuntime: false
 };
